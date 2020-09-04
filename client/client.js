79957bb72964a0450715461dc53baa3fad3c9d0b
Steve Doty <stevedoty16@gmail.com>
2020-09-04 16:24:57 -0700

    log first tweet object

---
+++ client/client.js
@@ -0,0 +1,17 @@
+console.log('hello');
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const name = formData.get('name');
+  const content = formData.get('content');
+
+  const tweet = {
+    name,
+    content
+  }
+  console.log(tweet);
+
+})
