051e348a8cddfe20935a203f3ddf611131e8a0f4
Steve Doty <stevedoty16@gmail.com>
2020-09-04 18:23:08 -0700

    pass tweet to 5000 & add nodemon

--- server/index.js
+++ server/index.js
@@ -2,6 +2,12 @@ const express = require('express');
 
 const app = express();
 
+app.get('/', (req, res) => {
+  res.json({
+    message: 'Tweet! 🐣'
+  });
+});
+
 app.listen(5000, () => {
   console.log('listening on http://localhost:5000');
 })
